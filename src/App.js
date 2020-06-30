b98c239a63c6607b1cf13515b8cb9c2aebdfa8c6
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-06-30 22:25:24 +0530

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div id="app">
+        <NavBar />
         <Home />
       </div>
     );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="nav-bar">
+      <div className="nav-bar-item">Home</div>
+      <div className="nav-bar-item">About</div>
+      <div className="nav-bar-item">Contact</div>
+    </nav>
+  );
+};
+export default NavBar;
