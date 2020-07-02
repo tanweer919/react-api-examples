c9af5ba3ae78fe797835092907af41d879f2985f
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-07-02 16:02:42 +0530

    Modified navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,48 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const links = [
+    { title: "Home", position: "left" },
+    { title: "About", position: "left" },
+    { title: "Contact", position: "left" },
+    { title: "Login", position: "right" },
+    { title: "Register", position: "right" },
+  ];
+  const handleClick = (message) => {
+    console.log(message);
+  };
   return (
     <nav className="nav-bar">
-      <div className="nav-bar-item">Home</div>
-      <div className="nav-bar-item">About</div>
-      <div className="nav-bar-item">Contact</div>
+      <div className="nav-bar-left">
+        {links
+          .filter((link) => link.position === "left")
+          .map((link, i) => (
+            <div
+              className="nav-bar-item"
+              onClick={() => {
+                handleClick(link.title);
+              }}
+              key={i}
+            >
+              {link.title}
+            </div>
+          ))}
+      </div>
+      <div className="nav-bar-right">
+        {links
+          .filter((link) => link.position === "right")
+          .map((link, i) => (
+            <div
+              className="nav-bar-item"
+              onClick={() => {
+                handleClick(link.title);
+              }}
+              key={i}
+            >
+              {link.title}
+            </div>
+          ))}
+      </div>
     </nav>
   );
 };
